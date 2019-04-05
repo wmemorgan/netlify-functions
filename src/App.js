81f20d8ad38d1e57abb96406b9503f5dc0edce29
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-05 18:39:48 +0800

    Initialize client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const AnotherPage = () => <h1>Another Page</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
+class Home extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.msg }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/.netlify/functions/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/.netlify/functions/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+
   render() {
     return (
       <div className="App">
@@ -19,10 +57,46 @@ class App extends Component {
           >
             Learn React
           </a>
+          <p>{this.state.response}</p>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Post to Server:</strong>
+            </p>
+            <input
+              type="text"
+              value={this.state.post}
+              onChange={e => this.setState({ post: e.target.value })}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.responseToPost}</p>
         </header>
       </div>
     );
   }
 }
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/another-page/">Another Page</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/another-page/" component={AnotherPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
